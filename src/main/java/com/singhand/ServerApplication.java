3cefa6d73f70681fa1146c3db3b21e889ee70d5e
admin <admin@DESKTOP-986C4RU>
2018-04-30 15:59:11 +0800

    first

---
+++ src/main/java/com/singhand/ServerApplication.java
@@ -0,0 +1,19 @@
+package com.singhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServerApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ServerApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/com/singhand/service/UserService.java
@@ -0,0 +1,9 @@
+package com.singhand.service;
+
+import org.springframework.stereotype.Service;
+
+@Service(value="UserService")
+public class UserService {
+
+	
+}
